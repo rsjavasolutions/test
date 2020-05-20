fcadcdaeb5ae56399581be1d4a15d6840468970d
rsjavasolutions <stan.robert.1986@gmail.com>
2020-05-20 10:59:17 +0200

    Task 6 finished, created 3 tests, 100% coverage

--- src/test/java/task6/ExceptionsExampleTest.java
+++ src/test/java/task6/ExceptionsExampleTest.java
@@ -3,6 +3,7 @@ package task6;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.*;
 import static task6.ExceptionsExample.DEFAULT_NUMBER;
 
@@ -31,4 +32,14 @@ class ExceptionsExampleTest {
         assertThat(string).isEqualTo(String.valueOf(DEFAULT_NUMBER));
     }
 
-}
+    @Test
+    void shouldThrowExceptionIfNumberIsGraterThanDefault(){
+        //given
+        int i = 10;
+
+        //when
+        assertThatThrownBy(() -> exceptionsExample.doSomething(i))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("Parametr jest większy od 5");
+        }
+    }
