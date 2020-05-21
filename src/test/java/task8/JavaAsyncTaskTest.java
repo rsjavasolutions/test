161cf7ea75d2071ccd440a721ae00395cd8946b8
rsjavasolutions <stan.robert.1986@gmail.com>
2020-05-21 10:58:39 +0200

    Asynhronic tests done

--- src/test/java/task8/JavaAsyncTaskTest.java
+++ src/test/java/task8/JavaAsyncTaskTest.java
@@ -1,7 +1,28 @@
 package task8;
 
+import org.awaitility.Awaitility;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.awaitility.Awaitility.await;
 import static org.junit.jupiter.api.Assertions.*;
 
 class JavaAsyncTaskTest {
+
+    private JavaAsyncTask javaAsyncTask = new JavaAsyncTask();
+
+    @Test
+    void shouldRunAsyncTask() {
+
+        //given
+
+        //when
+        javaAsyncTask.runTask();
+
+        //then
+        await().until(() -> javaAsyncTask.getResult());
+
+    }
+
 
 }
