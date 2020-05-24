91653c35c036cfb468175c9a1e0f6b1ebfc197c5
rsjavasolutions <stan.robert.1986@gmail.com>
2020-05-24 10:06:50 +0200

    New class UserService

---
+++ src/main/java/task10/service/UserService.java
@@ -0,0 +1,14 @@
+package task10.service;
+
+import task10.model.User;
+
+public class UserService {
+
+    public boolean existByUsername(String user){
+        throw new UnsupportedOperationException("Not implemented yet");
+    }
+
+    public User getUserByName(String user){
+        throw new UnsupportedOperationException("Not implemented yet");
+    }
+}
